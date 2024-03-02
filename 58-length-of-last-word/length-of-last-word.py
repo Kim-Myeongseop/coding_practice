8fc631e65289d4220d360767d887f9e01c42c64d
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-03-02 22:02:40 +0900

    Time: 44 ms (7.24%) | Memory: 16.6 MB (49.81%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,4 +1,4 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s_list = s.split()   # split(' ')를 하면 '' 들도 output으로 포함해서 분리함
-        return len(s_list[-1])
+        return len(s.split().pop())   # split(' ')를 하면 '' 들도 output으로 포함해서 분리함
+        
