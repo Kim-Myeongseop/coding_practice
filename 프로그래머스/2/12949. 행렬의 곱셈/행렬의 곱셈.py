d8d8ca667b709080aaae0204793c6edd1568411a
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-03-29 02:18:13 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.69 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    answer = [[0   for _ in range(len(arr2[0]))]   for _ in range(len(arr1))]
+    arr1   # m, n
+    arr2   # n, k
+    
+    for i in range(len(arr1)):   # 0~m-1
+        for k in range(len(arr2[0])):
+
+            for j in range(len(arr1[0])):   # 0~n-1
+                answer[i][k] += arr1[i][j] * arr2[j][k]
+                
+    return answer
