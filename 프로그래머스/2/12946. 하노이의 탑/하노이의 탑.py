120ffcfc69cf5c5732dd3d322d7cfdc41111e34c
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-04-04 12:18:37 +0900

    [level 2] Title: 하노이의 탑, Time: 18.34 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,15 +1,13 @@
 def solution(n):
-    def hanoi(num, start, end, temp):
+    def hanoi(num, start, end, temp):   # 원판의개수, 시작기둥, 목표기둥, 경유기둥
         if num == 1:
             return [[start,end]]
         
-        # n개를 1에서 3으로 옮기려면, n-1개를 1에서 2로 옮겨야한다.
         answer = []
-        answer.extend(hanoi(num-1, start, temp, end))
-        answer.extend(hanoi(1, start, end, temp))
-        answer.extend(hanoi(num-1, temp, end, start))
+        answer.extend(hanoi(num-1, start, temp, end))   # n-1개를 start에서 temp로 옮기기
+        answer.extend(hanoi(1, start, end, temp))   # 가장 큰 원반을 start에서 end로 옮기기
+        answer.extend(hanoi(num-1, temp, end, start))   # n-1개를 temp에서 end로 옮기기
         
         return answer
-            
     
     return hanoi(n, 1, 3, 2)
