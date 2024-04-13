3a19d1c5d126765daaa97d3bfabeeece46b23747
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-04-13 19:12:31 +0900

    [level 2] Title: 삼각 달팽이, Time: 176.23 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -10,13 +10,14 @@ def solution(n):
     total = n*(n+1)//2
     for num in range(1, total+1):
         # 할당 : 무조건 num은 하나 할당해야 끝난다.
-        if tower[r][c] == 0:
-            tower[r][c] = num
-        else:   # num은 무조건 할당해야하고 방향은 1번만 바뀐다.
-            d_idx = d_idx+1 if d_idx != 2 else 0
-            r += direction[d_idx][0]
-            c += direction[d_idx][1]
-            tower[r][c] = num
+        tower[r][c] = num
+        # if tower[r][c] == 0:
+        #     tower[r][c] = num
+        # else:   # num은 무조건 할당해야하고 방향은 1번만 바뀐다.
+        #     d_idx = d_idx+1 if d_idx != 2 else 0
+        #     r += direction[d_idx][0]
+        #     c += direction[d_idx][1]
+        #     tower[r][c] = num
         
         # 다음 인덱스 부여 : 문제 발생시 방향전환필요
         r += direction[d_idx][0]
