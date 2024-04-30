8a7a26b7c71955aa57612c8130a10535ed0b2aff
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-04-30 15:53:14 +0900

    Time: 1388 ms (22.68%) | Memory: 49.3 MB (6.83%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,8 +1,5 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        res = 0
-        max_idx = 0
-        min_idx = 0
         max_list = []
         min_list = []
         for i, array in enumerate(arrays):
@@ -10,8 +7,6 @@ class Solution:
             min_list.append((i, array[0]))
         max_list.sort(key=lambda x: x[1])
         min_list.sort(key=lambda x: x[1], reverse=True)
-        print(max_list)
-        print(min_list)
         if max_list[-1][0] == min_list[-1][0]:
             res = max(max_list[-2][1] - min_list[-1][1], max_list[-1][1] - min_list[-2][1])
         else:
