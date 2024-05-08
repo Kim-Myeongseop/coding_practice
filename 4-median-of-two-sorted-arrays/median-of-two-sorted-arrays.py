fc4541dfde158a8809750af10ffc80be8e5fa7e2
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-05-09 04:33:49 +0900

    Time: 81 ms (55.93%) | Memory: 16.9 MB (34.08%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -11,8 +11,8 @@ class Solution:
         while low <= high:
             mid1 = (low + high) // 2
             mid2 = left - mid1
-            l1 = float('-inf') if mid1 == 0 else nums1[mid1 - 1]
-            l2 = -float('inf') if mid2 == 0 else nums2[mid2 - 1]
+            l1 = float('-inf') if mid1 == 0 else nums1[mid1-1]
+            l2 = -float('inf') if mid2 == 0 else nums2[mid2-1]
             r1 = math.inf if mid1 == len1 else nums1[mid1]
             r2 = math.inf if mid2 == len2 else nums2[mid2]
             if l1 <= r2 and l2 <= r1:
