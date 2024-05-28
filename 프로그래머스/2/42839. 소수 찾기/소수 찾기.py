0c0d7bc10738889ca235ee8de515425170626f12
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-05-28 14:43:40 +0900

    [level 2] Title: 소수 찾기, Time: 1.17 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -15,24 +15,20 @@ def isprime(num):
     for n in range(2, sqrt+1):
         if num/n == int(num/n):
             sign = False
-    return sign if num != 1 else False
+    return sign if num > 1 else False
 
 def solution(numbers):
     answer = 0
     numbers = [number for number in numbers]
     num_list = []
-    
     for i in range(1, len(numbers)+1):
         fn = permutation(numbers, i)
         for item in fn:
             num_list.append(''.join(item))
-    
     num_list = list(set(num_list))
     
     for num in num_list:
-        if num == '2':
-            answer += 1
-        elif num[0] == '0' or num[-1] in ['0','2','4','6','8']:   # 검사 불필요
+        if num[0] == '0' or num != '2' and num[-1] in ['0','2','4','6','8']:   # 검사 불필요
             continue
         else:
             if isprime(int(num)):
