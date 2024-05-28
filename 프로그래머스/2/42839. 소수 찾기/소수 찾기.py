5ec8798ee5200c64c4c9718fd2a5dd76e79cd9c4
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-05-28 14:28:59 +0900

    [level 2] Title: 소수 찾기, Time: 2.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,40 @@
+def permutation(arr, r=None):
+    if r == None:
+        r = len(arr)
+        
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for new_arr in permutation(arr[:i]+arr[i+1:], r-1):
+                yield [arr[i]] + new_arr
+
+def isprime(num):
+    sign = True
+    sqrt = round(num**(1/2))
+    for n in range(2, sqrt+1):
+        if num/n == int(num/n):
+            sign = False
+    return sign
+
+def solution(numbers):
+    answer = 0
+    numbers = [number for number in numbers]
+    num_list = []
+    
+    for i in range(1, len(numbers)+1):
+        fn = permutation(numbers, i)
+        for item in fn:
+            num_list.append(''.join(item))
+    
+    num_list = list(set(num_list))
+    
+    for num in num_list:
+        if num == '2':
+            answer += 1
+        elif num[0] == '0' or num[-1] in ['0','2','4','6','8'] or num == '1':   # 검사 불필요
+            continue
+        else:
+            if isprime(int(num)):
+                answer += 1
+    return answer
