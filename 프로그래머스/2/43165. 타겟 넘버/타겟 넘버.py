9078d1c00b44544b23b5435cba12a7d6287a7ec6
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-05-30 16:53:39 +0900

    [level 2] Title: 타겟 넘버, Time: 15.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -22,3 +22,32 @@ def solution(numbers, target):
         if pointers == [1]*len(numbers):
             break
     return answer
+
+'''
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+'''
+    
+'''
+answer = 0
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
+'''
