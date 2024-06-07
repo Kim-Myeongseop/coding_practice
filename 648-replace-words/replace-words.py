ecf0b6c4f8c2393e5d89507fb0718374ffbc0e2c
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-07 18:18:28 +0900

    Time: 2769 ms (5.03%) | Memory: 24.9 MB (94.66%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,22 @@
+class Solution:    
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        answer = []
+        dictionary = sorted(dictionary, key=lambda x: len(x))
+        sentence = sentence.split()
+        for word in sentence:   # cattle
+            for root in dictionary:
+                if len(root) > len(word):
+                    sign = False
+                    continue
+                sign = True
+                for i in range(len(root)):
+                    if root[i] != word[i]:
+                        sign = False
+                        break
+                if sign == True:
+                    answer.append(root)
+                    break
+            if sign == False:
+                answer.append(word)
+        return ' '.join(answer)
+
