d98184b65a5974993757a1e35f48536afded5a93
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-14 12:37:21 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,18 @@
+# def solution(numbers):
+#     answer = []
+#     numbers = sorted(map(str, numbers), reverse=True)
+#     for number in numbers:
+#         temp = []
+#         while answer and answer[-1] + number < number + answer[-1]:
+#             temp.append(answer.pop())
+#         answer.append(number)
+#         while temp:
+#             answer.append(temp.pop())
+#     answer = ''.join(answer)
+#     return str(int(answer))
+
+# 다른 풀이 1
 def solution(numbers):
-    answer = []
-    numbers = sorted(map(str, numbers), reverse=True)
-    for number in numbers:
-        temp = []
-        while answer and answer[-1] + number < number + answer[-1]:
-            temp.append(answer.pop())
-        answer.append(number)
-        while temp:
-            answer.append(temp.pop())
-    answer = ''.join(answer)
-    return str(int(answer))
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*4, reverse=True)
+    return str(int(''.join(numbers)))
