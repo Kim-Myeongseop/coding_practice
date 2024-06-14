870eda9f554060b9359b26b11b129a2ee6606daf
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-14 12:38:19 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,18 +1,18 @@
-# def solution(numbers):
-#     answer = []
-#     numbers = sorted(map(str, numbers), reverse=True)
-#     for number in numbers:
-#         temp = []
-#         while answer and answer[-1] + number < number + answer[-1]:
-#             temp.append(answer.pop())
-#         answer.append(number)
-#         while temp:
-#             answer.append(temp.pop())
-#     answer = ''.join(answer)
-#     return str(int(answer))
-
-# 다른 풀이 1
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*4, reverse=True)
-    return str(int(''.join(numbers)))
+    answer = []
+    numbers = sorted(map(str, numbers), reverse=True)
+    for number in numbers:
+        temp = []
+        while answer and answer[-1] + number < number + answer[-1]:
+            temp.append(answer.pop())
+        answer.append(number)
+        while temp:
+            answer.append(temp.pop())
+    answer = ''.join(answer)
+    return str(int(answer))   # 0 으로 시작 제거
+
+# # 다른 풀이 1
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
