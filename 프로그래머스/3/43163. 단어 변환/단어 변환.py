df3d49994a4c8f09f9fc560d2ca6cc8eac701a06
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-17 13:56:09 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -24,21 +24,36 @@ def solution(begin, target, words):
             if diff_cnt == 1:
                 linked[word].append(w)
     
-    # DFS(Depth-First Search)
     search_list = deque()
     search_list.append(begin)
     cnt = 0
-    while search_list:
-        word = search_list.pop()
-        next_words = linked[word]
-        if next_words:
+    while search_list and cnt <= len(words):
+        temp = []
+        for word in search_list:
+            next_words = linked[word]
             for next_word in next_words:
-                if visited[next_word] == False:   # 방문한 적 없으면
-                    search_list.append(next_word)
-                    visited[next_word] = True
+                if next_word not in temp:
+                    temp.append(next_word)
         cnt += 1
-        if target in search_list:
-            return cnt
+        search_list = temp[:]
+        if target in temp:
+            answer = cnt
+            break
+#     # DFS(Depth-First Search)
+#     search_list = deque()
+#     search_list.append(begin)
+#     cnt = 0
+#     while search_list:
+#         word = search_list.pop()
+#         next_words = linked[word]
+#         if next_words:
+#             for next_word in next_words:
+#                 if visited[next_word] == False:   # 방문한 적 없으면
+#                     search_list.append(next_word)
+#                     visited[next_word] = True
+#         cnt += 1
+#         if target in search_list:
+#             return cnt
     
     # BFS(Breadth-First Search)
     return answer
