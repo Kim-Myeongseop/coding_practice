0e9ea7658791890c6cf5a5cdfbb6bdbb7e189311
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-20 13:48:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -7,29 +7,20 @@ def solution(operations):
             if tree:
                 visit = root
                 while visit:
-                    # idx = 0 if current < visit else 1
-                    if current < visit:   # 왼쪽 노드로 보낸다.
-                        idx = 0
-                        if tree[visit][0] == None:
-                            tree[visit][0] = current
-                            tree[current] = [None, None]
-                            break
-                        else:
-                            visit = tree[visit][0]
-                    else:   # 오른쪽 노드로 보낸다.
-                        if tree[visit][1] == None:
-                            tree[visit][1] = current
-                            tree[current] = [None, None]
-                            break
-                        else:
-                            visit = tree[visit][1]
+                    idx = 0 if current < visit else 1   # 왼쪽(0) or 오른쪽(1)
+                    if tree[visit][idx] == None:
+                        tree[visit][idx] = current
+                        tree[current] = [None, None]
+                        break
+                    else:
+                        visit = tree[visit][idx]
             else:
                 tree[current] = [None, None]
                 root = current
         
         else:
             # 최대값 명령
-            if operation[-2:] == " 1" and tree:
+            if operation.split()[-1] == "1" and tree:
                 visit = root
                 parent = False
                 # 조회
@@ -48,7 +39,7 @@ def solution(operations):
                 tree.pop(visit)
                         
             # 최소값 명령
-            elif operation[-2:] == "-1" and tree:
+            elif operation.split()[-1] == "-1" and tree:
                 print('ok')
                 visit = root
                 parent = False
@@ -64,7 +55,7 @@ def solution(operations):
                     if tree[visit][1] != None:
                         root = tree[visit][1]
                 else:
-                    tree[parent][0] = tree[visit][1]   # 자식 노드의 왼쪽 첫번째 노드 연결 없으면 None
+                    tree[parent][0] = tree[visit][1]   # 자식 노드의 우측 첫번째 노드 연결 없으면 None
                 tree.pop(visit)
     if tree:
         answer = [max(tree), min(tree)] 
