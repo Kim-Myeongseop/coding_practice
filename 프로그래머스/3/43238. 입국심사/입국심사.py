5f2e82f769518417655272cc59ebaa9dad562057
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-21 13:11:57 +0900

    [level 3] Title: 입국심사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -15,4 +15,5 @@ def solution(n, times):
         else:
             l = mid + 1   # 이 경우 l, r이 연속으로 있다면, 답이 r일 경우 l이 r로 가버리지만 while 문이 돌아가지 않아 mid가 초기화 될 수 없다 따라서 return mid 대신 return l을 하는 것이 맞다.
     answer = min(answer)
-    return answer
+    return l
+    #return answer
