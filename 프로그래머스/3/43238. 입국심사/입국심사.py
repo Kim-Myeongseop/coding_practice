dbddc1f5367a0277890e1974552536ac22c7b22b
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-06-21 13:06:59 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -3,14 +3,14 @@ def solution(n, times):
     # 시간 자체를 변수로 예측하는 방법. 이분 탐색으로 최적 점을 찾자.
     l = 0
     r = max(times)*n
-    while l <= r:
+    while l < r:
         total_people = 0
         mid = (l+r)//2
         for time in times:
             total_people += mid//time
             
         if total_people >= n:
-            r = mid - 1
+            r = mid
             answer.append(mid)
         else:
             l = mid + 1
