8dee370d051b96d3741b1bc0391f9d7a23ada69a
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-07-02 15:07:06 +0900

    Time: 57 ms (21.07%) | Memory: 16.7 MB (71.02%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,9 +1,8 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         answer = []
-        nums1_, nums2_ = nums1[:], nums2[:]
-        for num in nums1_:
-            if num in nums2_:
+        for num in nums1:
+            if num in nums2:
                 answer.append(num)
-                nums2_.remove(num)
+                nums2.remove(num)
         return answer
