139d0a5a1fe50eda02770193762c3f1cfd92b7e6
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-07-03 19:26:26 +0900

    [level 2] Title: 전화번호 목록, Time: 85.30 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].find(phone_book[i]) == 0:
+            answer = False
+            break
+    return answer
