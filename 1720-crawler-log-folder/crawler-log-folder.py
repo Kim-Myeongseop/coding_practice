fdc1151ffedbd1fd3bfed0eac6145601df14becb
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-07-10 12:33:44 +0900

    Time: 45 ms (81.19%) | Memory: 16.8 MB (11.07%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = deque()
+        for log in logs:
+            if log == '../':
+                if stack:
+                    stack.pop()
+            elif log == './':
+                continue
+            else:
+                stack.append(log)
+        return len(stack)
+                
