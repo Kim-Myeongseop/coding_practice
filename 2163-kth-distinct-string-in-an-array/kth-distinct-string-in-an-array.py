dc48e7b39f465761e89480b43bc3fcdc6961fbad
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-08-06 00:24:05 +0900

    Time: 129 ms (29.83%) | Memory: 16.7 MB (75.33%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.py
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.py
@@ -7,7 +7,7 @@ class Solution:
                 distincts.remove(a)
                 remove_list.append(a)
             else:
-                if a not in remove_list:
+                if a not in remove_list:   # 삭제 됐다가 다시 추가할 때, 이전에 지웠는지를 확인해야함
                     distincts.append(a)
         
         if k <= len(distincts):
