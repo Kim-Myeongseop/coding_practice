fb68e4690139be6461ccf883c788514facc4db17
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-08-06 22:45:18 +0900

    Time: 42 ms (30.25%) | Memory: 16.4 MB (74.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,16 +10,15 @@ class Solution:
         while list1 != None and list2 != None:
             if list1.val <= list2.val:
                 current_node.next = ListNode(list1.val)
-                current_node = current_node.next
+                current_node = current_node.next   # 얕은 복사
                 list1 = list1.next
             else:
                 current_node.next = ListNode(list2.val)
-                current_node = current_node.next
+                current_node = current_node.next   # 얕은 복사
                 list2 = list2.next
         
         if list1 == None:
             current_node.next = list2
         else:
             current_node.next = list1
-        
         return main_node.next
