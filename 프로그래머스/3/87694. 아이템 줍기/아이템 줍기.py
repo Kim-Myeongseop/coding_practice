198de82b3568ded4e9722bcb8b4042d77b9586db
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-08-27 13:44:14 +0900

    [level 3] Title: 아이템 줍기, Time: 2.21 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0   # 거리
+    
+    # board 생성(0:inner, 1:boundary, -1:outer)
+    board = [[-1 for _ in range(101)] for _ in range(101)]
+    for x_min, y_min, x_max, y_max in rectangle:
+        for r in range(2*(50-y_max), 2*(50-y_min)+1):
+            for c in range(2*x_min, 2*x_max+1):
+                if r == 2*(50-y_max) or r == 2*(50-y_min) or c == 2*x_min or c == 2*x_max:
+                    if board[r][c] != 0:   # 이미 이전의 내부점으로 된 점은 제외
+                        board[r][c] = 1
+                else:
+                    board[r][c] = 0
+                    
+    # search
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]
+    q = deque()
+    q.append((characterX, characterY))
+    visited = []   # 어차피 모든 길은 2개의 선택지 뿐이다. 즉, list로 관리할 필요가 없다.
+    item_cnt = 1e+8
+    while q:
+        x, y = q.pop()
+        if (x, y) == (itemX, itemY):
+            item_cnt = answer
+            answer = 0
+        for direction in directions:
+            nx = x + direction[0]
+            ny = y + direction[1]
+            cx = (2*nx + 2*x)//2
+            cy = (2*(50-ny) + 2*(50-y))//2
+            if (0 <= nx <= 50) and (0 <= ny <= 50) and (board[2*(50-ny)][2*nx] == 1) and (board[cy][cx] == 1) and (nx,ny) not in visited:
+                q.append((nx, ny))
+                answer += 1
+                break
+        visited.append((x, y))
+        # 재도착지점 직전에 끝나기 때문에 재도착지점 직전에서 재도착지점까지 거리를 추가해준다.
+        if item_cnt < answer:
+            break
+    # 재도착지점 직전에 끝나기 때문에 재도착지점 직전에서 재도착지점까지 거리를 추가해준다.
+    answer += 1
+    return min(answer, item_cnt)
