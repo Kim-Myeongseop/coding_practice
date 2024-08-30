fcc14c0dfb19d4bfc2e28023980bcbea6332c9f0
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-08-30 09:27:35 +0900

    [level 3] Title: 정수 삼각형, Time: 53.41 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -6,12 +6,18 @@ def solution(triangle):
             if i == 0:
                 prev_num = 0
             else:
-                if j == 0:
+                if j == 0:   # 가장 왼쪽 값 : 선택할 수 있는 경우의 수 1개
                     prev_num = sum_list[i-1][j]
-                elif j == i:
+                elif j == i:   # 가장 오른쪽 값 : 선택할 수 있는 경우의 수 1개
                     prev_num = sum_list[i-1][j-1]
-                else:
+                else:   # 중간 : 2개 중 큰 값 선택
                     prev_num = max(sum_list[i-1][j-1], sum_list[i-1][j])
             temp.append(triangle[i][j] + prev_num)
         sum_list.append(temp)
     return max(temp)
+
+'''
+위에서 부터 가능한 최대값을 아래 수로 더해줌
+7 / 3 8 / 8 1 0 -> 10 15 / 8 1 0 -> 18 16 15
+즉, 현재 row의 값들까지 오는 경로 중 최대값으로 교체하는 것
+'''
