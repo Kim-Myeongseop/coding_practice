a78390f6c3332be0e576cfcedccb52821229a4ee
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-08-31 16:48:02 +0900

    [level 2] Title: 피로도, Time: 42.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -20,3 +20,26 @@ def solution(k, dungeons):
                 break
         answer = max(answer, cnt)
     return answer
+
+# 다른 풀이 1
+# answer = 0
+# N = 0
+# visited = []
+
+# def dfs(k, cnt, dungeons):
+#     global answer
+#     if cnt > answer:
+#         answer = cnt
+
+#     for j in range(N):
+#         if k >= dungeons[j][0] and not visited[j]:   # cnt를 추가할 수 있는 조건
+#             visited[j] = 1   # 방문 처리
+#             dfs(k - dungeons[j][1], cnt + 1, dungeons)   # cnt 추가 후 다음 단계 진행
+#             visited[j] = 0   # 위의 탐색이 종료됐으면 다음 탐색을 위해 미방문 처리 후 for문 이어서
+
+# def solution(k, dungeons):
+#     global N, visited
+#     N = len(dungeons)
+#     visited = [0] * N
+#     dfs(k, 0, dungeons)
+#     return answer
