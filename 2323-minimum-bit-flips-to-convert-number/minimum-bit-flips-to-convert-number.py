6a53982fca6ef7a974e5cbc4887e5e425359e5b0
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-09-11 15:11:35 +0900

    Time: 33 ms (73.24%) | Memory: 16.6 MB (26.96%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,3 +1,14 @@
+# class Solution:
+#     def minBitFlips(self, start: int, goal: int) -> int:
+#         return sum(map(int, bin(start ^ goal)[2:]))
+
+# 다른 풀이 1
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        return sum(map(int, bin(start ^ goal)[2:]))
+        cnt = 0
+        while start or goal:
+            if start%2 != goal%2:   # 같지 않으면 바꿔야한다.
+                cnt += 1
+            start = start//2
+            goal = goal // 2
+        return cnt
