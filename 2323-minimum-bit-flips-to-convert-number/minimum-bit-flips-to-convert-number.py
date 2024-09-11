00996931dc6eb46d30baca90ec950e88804e988a
kmscopragit <139935129+kmscopragit@users.noreply.github.com>
2024-09-11 15:08:17 +0900

    Time: 31 ms (81.69%) | Memory: 16.3 MB (93.56%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,5 +1,3 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        answer = start ^ goal
-        answer = bin(answer)[2:]
-        return sum(map(int, answer))
+        return sum(map(int, bin(start ^ goal)[2:]))
